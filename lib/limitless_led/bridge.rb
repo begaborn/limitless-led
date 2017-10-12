18354457a7ec6e1a92ec78e956dc86fb014bc362
shinhyounsub <shinhyounsub@MacBook-Pro.local>
2017-10-13 00:47:16 +0900

    Add White Led

--- lib/limitless_led/bridge.rb
+++ lib/limitless_led/bridge.rb
@@ -1,25 +1,6 @@
 module LimitlessLed
   class Bridge
     include Colors
-
-    COMMANDS = {
-      all_off: 65,
-      all_on: 66,
-      disco: 77,
-      disco_slower: 67,
-      disco_faster: 68,
-      color: 64,
-      brightness: 78,
-      group_1_on: 69,
-      group_1_off: 70,
-      group_2_on: 71,
-      group_2_off: 72,
-      group_3_on: 73,
-      group_3_off: 74,
-      group_4_on: 75,
-      group_4_off: 76
-    }
-
     attr_accessor :host, :port
 
     def initialize(host: 'localhost', port: 8899)
@@ -37,10 +18,6 @@ module LimitlessLed
           socket.connect host, port
         end
       end
-    end
-
-    COMMANDS.each do |cmd, _|
-      define_method(cmd) { command cmd }
     end
 
     def white
@@ -69,7 +46,7 @@ module LimitlessLed
     end
 
     def command(command_key, command_param = 0)
-      send_packet COMMANDS[command_key].chr + command_param.chr + 85.chr
+      send_packet self.class::COMMANDS[command_key].chr + command_param.chr + 85.chr
     end
 
     def go_crazy
@@ -85,4 +62,131 @@ module LimitlessLed
       end
     end
   end
+
+  class RGBW < Bridge
+    COMMANDS = {
+      all_off: 65,
+      all_on: 66,
+      disco: 77,
+      disco_slower: 67,
+      disco_faster: 68,
+      color: 64,
+      brightness: 78,
+      group_1_on: 69,
+      group_1_off: 70,
+      group_2_on: 71,
+      group_2_off: 72,
+      group_3_on: 73,
+      group_3_off: 74,
+      group_4_on: 75,
+      group_4_off: 76
+    }
+
+    COMMANDS.each do |cmd, _|
+      next if cmd == :color || cmd == :brightness
+
+      define_method(cmd) { command cmd }
+    end
+
+    def color(color)
+      color_code = if color.is_a?(Color::RGB)
+         color_code_from_color(color)
+       elsif color.is_a?(Integer)
+         color
+       elsif color.is_a?(String)
+         color_code_from_color Color::RGB.const_get(color.camelize)
+       end
+
+      command :color, color_code
+    end
+
+
+  end
+
+  class White < Bridge
+    COMMANDS = {
+      all_off: 0x39,
+      all_on: 0x35,
+      brightness_up: 0x3c,
+      brightness_down: 0x34,
+      all_brightness_max: 0xb5,
+      all_night_mode: 0xbb,
+      warm_increase: 0x3e,
+      cool_increase: 0x3f,
+      group_1_on: 0x38,
+      group_1_off: 0x3b,
+      group_2_on: 0x3d,
+      group_2_off: 0x33,
+      group_3_on: 0x37,
+      group_3_off: 0x3a,
+      group_4_on: 0x32,
+      group_4_off: 0x36,
+      group_1_brightness_max: 0xb8,
+      group_1_night_mode: 0xbb,
+      group_2_brightness_max: 0xbd,
+      group_2_night_mode: 0xb3,
+      group_3_brightness_max: 0xb7,
+      group_3_night_mode: 0xba,
+      group_4_brightness_max: 0xb2,
+      group_4_night_mode: 0xb6
+    }
+
+    GROUPS = [
+      'group_1',
+      'group_2',
+      'group_3',
+      'group_4'
+    ]
+
+    COMMANDS.each do |cmd, _|
+      define_method(cmd) {
+        command cmd
+      }
+    end
+
+    GROUPS.each do |grp|
+      define_method("#{grp}_brightness_min".to_sym) {
+        send("#{grp}_on".to_sym)
+        brightness_min
+      }
+    end
+
+    GROUPS.each do |grp|
+      define_method("#{grp}_brightness_up".to_sym) {
+        send("#{grp}_on".to_sym)
+        brightness_up
+      }
+    end
+
+    GROUPS.each do |grp|
+      define_method("#{grp}_brightness_down".to_sym) {
+        brightness_down
+      }
+    end
+
+    GROUPS.each do |grp|
+      define_method("#{grp}_warm".to_sym) {
+        warm_increase
+      }
+    end
+
+    GROUPS.each do |grp|
+      define_method("#{grp}_cool".to_sym) {
+        cool_increase
+      }
+    end
+
+    def brightness_min
+      10.times {
+        sleep 15/100.0
+        brightness_down
+      }
+    end
+
+    def all_brightness_min
+      all_on
+      brightness_min
+    end
+  end
+
 end
